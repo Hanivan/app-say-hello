067d8518be46249f9436b0edac931bce00855016
Hanivan <hanivan20@gmail.com>
2021-08-18 17:28:31 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Hanivan/app-say-hello
+
+go 1.17
+
+require github.com/Hanivan/go-say-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/Hanivan/go-say-hello/v2"
+)
+
+func main() {
+
+	fmt.Println(gosayhello.SayHello("Haniv"))
+
+}
